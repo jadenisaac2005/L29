ec50aee46f91ce38abe352f498232e9abb07b59f
Jaden Isaac <jadenisaac10@gmail.com>
2025-03-09 23:24:00 +0530

    update new.c

--- new.c
+++ new.c
@@ -5,6 +5,11 @@ printf("Hi");
 printf("I");
 printf("am');
 printf("Jaden");
+<<<<<<< HEAD
 printf('Hi I am Jaden');
+=======
+printf("Isaac");
+scanf("");
+>>>>>>> 3c2fa07ebb9090f2bedeaac93b49bf9ee3862081
 return 0;
 }
